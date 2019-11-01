d40c543c1485df5cf6de241c56427d3b78b8f169
Pavel Popov1 <Pavel_Popov1@epam.com>
2019-11-01 20:46:17 +0400

    docker installing

---
+++ docker-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io
+yum systemctl start docker
