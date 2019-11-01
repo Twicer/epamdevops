a53874cad49df0aa3db151b9dced8f1ec54f072d
Pavel Popov1 <Pavel_Popov1@epam.com>
2019-11-01 20:17:09 +0400

    nfs client installing

--- nfs-client.sh
+++ nfs-client.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-yum update && yum install nfs-utils
+#yum update
+yum install nfs-utils -y
 mkdir -p /mnt/nfs/home && mkdir -p /mnt/nfs/var/nfs-share
+systemctl start nfs && systemctl enable nfs
 
 systemctl enable rpcbind
 systemctl enable nfs-server
@@ -9,3 +11,8 @@ systemctl enable nfs-lock
 systemctl start rpcbind
 systemctl start nfs-server
 systemctl start nfs-lock
+
+mount -t nfs 10.18.0.1:/home /mnt/nfs/home/
+mount -t nfs 10.18.0.1:/var/nfsshare /mnt/nfs/var/nfsshare/
+
+
